c36867c8c3fbfd7607064ba194e356bdad36bf27
Daniel-LSP <gcinovation12@gmail.com>
2025-03-14 19:08:45 -0500

    introduccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,39 @@
+# Autor: Lewin Daniel 
+# Este es un comentario de una sola linea 
+'''
+Este es un comentario de 
+multiples 
+lineas
+
+'''
+
+print("Estudiando python")
+#Definicion de variables 
+'''
+Reglas: 
+Python es sensible a minusculas y mayusculas
+No se pueden usar espacios
+No se pueden usar palabras reservadas
+No es conveniente usar numeros como nombres de variables
+
+'''
+x=50
+y=15
+print(x+y)  
+print(type(x))
+# <class 'int'> : significa que x es un numero entero
+
+nombre="Andres"
+print(nombre)
+print(type(nombre))
+#<class 'str'> : significa que la variable nombre es un string (cadena)
+
+print(type(x))
+x=4.8
+print(type(x))
+#<class 'float'> : significa que la variable es un flotante (numero decimal)
+
+print(x , nombre)
+
+my_bool: "8" = "9"
+print(my_bool)
