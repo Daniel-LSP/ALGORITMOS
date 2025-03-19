8c535bc86b4ec1a108e32f506c87171c5b41b511
Daniel-LSP <gcinovation12@gmail.com>
2025-03-19 18:26:17 -0500

    introduccion a python

--- uso_de_python.py
+++ uso_de_python.py
@@ -36,3 +36,72 @@ print(type(x))
 print(x , nombre)
 
 
+
+#Variables 
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# Concatenacion de varaibles en un print 
+print(my_string_variable, my_int_variable, my_bool_variable)
+print("Ese es el valor de:", my_bool_variable)
+
+# Algunas funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola linea
+name, surname, alias, age = "Lewin", "Daniel", "Flash", 18
+print("Me llamo:", name, surname, ".Mi edad es:", age, ".Y mi alias es:", alias)
+
+#Inputs
+"""
+
+name = input('¿Cual es tu nombre? ')
+age = input('¿Cuantos años tienes? ')
+print(name)
+print(age)
+
+"""
+#Cambiamos su tipo
+name = 18
+age = "Lewin"
+print(name)
+print(age)
+
+### Operadores ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3)
+print("Hola" +" ""Python")
+print("Hola" == "python")
+print("Hola" < "Python")
+print("Hola" > "Python")
+print("Hola" >= "Python")
+print("Hola" * 5)
+print("Hola" + str(5))
+
+
+
+
+
+
+
+
+
+
+
