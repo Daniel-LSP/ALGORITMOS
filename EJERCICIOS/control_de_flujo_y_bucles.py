72a6ef426b72b8b9b544c15dda30c095271fab23
Daniel-LSP <gcinovation12@gmail.com>
2025-03-28 19:59:25 -0500

    clase

---
+++ EJERCICIOS/control_de_flujo_y_bucles.py
@@ -0,0 +1,61 @@
+'''
+condicional "if then else"
+"si se cumple una condicion 
+entonces se realiza una operacion +de lo contrario, se realiza 
+otra opcion (opcionalmente) 
+'''
+
+# x = 800
+# y = 200
+# if x > y: #condicion
+#     print("x es mayor que y") #operacion
+
+#uso del control if-else
+
+# w = 500
+# z = 200
+# if w > z: 
+#     print("w es mayor que z")
+# else:
+#     print("w es menor que z")
+
+#uso del control if-elif-else
+
+w = 500
+z = 200
+if w > z: 
+    print("w es mayor que z")
+elif w==500:
+    print("el valor de w es 500")#condicion 2..
+elif z==200:
+    print("el valor de z es 200")
+else:
+    print("w es menor que z")
+'''
+elif sirve para evaluar multiples condiciones
+'''
+
+#uso de if anidados
+# usuario=input("por favor ingrese su usuario: ")
+
+# if usuario=="Andres":
+#     password=input("por favor ingrese su contraseña: ")
+#     if password=="1234":
+#         print("usuario puede acceder")
+#     else:
+#         print("La contraseña es incorrecta")
+# else:
+#     print("el usuario no existe")
+
+#---------
+# usuario=input("por favor ingrese su usuario: ")
+# password=input("por favor ingrese su contraseña: ")
+
+# if usuario=="Andres" and password=="1234":
+#     print("usuario puede acceder")
+# else:
+#     print("el usuario o la contraseña no es valida")
+
+
+
+
